67c6f0d63396ead48009ea1276c442e67982624e
YO0920 <sskym712325@gmail.com>
2024-03-09 14:32:33 +0900

    first commit

---
+++ js/firebaseApikey.js
@@ -0,0 +1,10 @@
+        // Your web app's Firebase configuration
+        export default {
+            apiKey: "AIzaSyBgnga1viNXfczOwERt-Bh19-iCmGNKsuI",
+            authDomain: "sample-67315.firebaseapp.com",
+            databaseURL: "https://sample-67315-default-rtdb.firebaseio.com",
+            projectId: "sample-67315",
+            storageBucket: "sample-67315.appspot.com",
+            messagingSenderId: "110470410966",
+            appId: "1:110470410966:web:1c3f88559582fdbd8bcf77"
+        };
